67b8d8b11988249dad9b9951aef59b2a67e25e1c
Andrey <andriy09082@gmail.com>
2020-06-24 23:46:40 +0300

    Это наш первый комит

---
+++ app.js
@@ -0,0 +1,14 @@
+// 1. PEremennie
+
+var name = 'Andrey';
+const lastName = 'Andruha';
+let age = 20;
+const isProgrammer = ture
+
+name = 'And'
+
+console.log(name)
+
+// 2. Mutirovanie
+
+console.log('Имя человека: ' +)
